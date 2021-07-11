df8753c14c394e2f57e7e2f9e6dcb6885983306d
Boiko Roman <boykoroman94@gmail.com>
2021-07-11 16:43:56 +0300

    добавил скрол

--- src/apiService.js
+++ src/apiService.js
@@ -1,3 +1,6 @@
+const API_KEY = '22353623-6201980fc547853fa44be7702';
+const BASE_URL = 'https://pixabay.com/api';
+
 export default class ImagesApiService {
     constructor() {
         this.searchQuery = '';
@@ -6,13 +9,13 @@ export default class ImagesApiService {
 
     fetchImages() {
 
-        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=22353623-6201980fc547853fa44be7702`;
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
 
         return fetch(url)
-            .then(r => r.json())
-            .then(data => {
-                this.incrementPage()
-                return data.hits;
+            .then(response => response.json())
+            .then(({ hits }) => {
+                this.incrementPage();
+                return hits;
             });
     }
 
--- src/index.js
+++ src/index.js
@@ -3,21 +3,42 @@ import imagesTemplate from "./images.hbs";
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
-    imagesList: document.querySelector('.gallery')
+    imagesList: document.querySelector('.gallery'),
+    anchor: document.querySelector('.anchor')
 }
 
 const imagesApiService = new ImagesApiService();
+const observer = new IntersectionObserver(loadMore, {
+    threshold: 0,
+});
 
-refs.searchForm.addEventListener('submit', handleInput);
 
-function handleInput(e) {
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
     e.preventDefault();
 
     imagesApiService.query = e.currentTarget.elements.query.value;
+    if (imagesApiService.query === '') {
+        return alert('Enter query to search');
+    }
     imagesApiService.resetPage();
-    imagesApiService.fetchImages().then(apppendMarkup);
+    imagesApiService.fetchImages().then(images => {
+        clearGallery();
+        apppendMarkup(images);
+        observer.observe(refs.anchor);
+    });
 }
 
 function apppendMarkup(images) {
     refs.imagesList.insertAdjacentHTML('beforeend', imagesTemplate(images));
+}
+
+function clearGallery() {
+    refs.imagesList.innerHTML = '';
+}
+
+function loadMore() {
+    imagesApiService.fetchImages().then(apppendMarkup);
 }
